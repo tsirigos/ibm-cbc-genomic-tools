f2e1abab8e77ed5d22e0a02675b66d9625fbdf97
tsirigos@gmail.com <tsirigos@gmail.com@73350a43-d524-bd0f-40ca-bdf4196cea37>
2012-03-19 22:54:27 +0000

    [UPDATE] genomic_apps heatmap: updated built-in template for multi-color support

--- trunk/src/genomic_intervals.h
+++ trunk/src/genomic_intervals.h
@@ -4,7 +4,7 @@
 // which accompanies this distribution, and is available at http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 //
 
-const string VERSION = "genomic_tools 2.1.0 [under development; revision 4]";
+const string VERSION = "genomic_tools 2.1.0 [under development; revision 5]";
 
 
 #include <stdio.h>
